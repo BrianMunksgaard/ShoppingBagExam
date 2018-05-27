f014a8c821c85e4ec0451555dee543a56869ab82
Jens Christian Rasch <jcr@wodecraft.dk>
2018-05-27 17:04:51 +0200

    Added comments

--- app/src/main/java/com/exam/shoppingbagexam/AboutFragment.java
+++ app/src/main/java/com/exam/shoppingbagexam/AboutFragment.java
@@ -8,10 +8,18 @@ import android.view.ViewGroup;
 
 
 /**
- *
+ * Fragment used for showing the about page for the shopping list app.
  */
 public class AboutFragment extends Fragment {
 
+    /**
+     * Will create the About view based on the layout specified in the fragment_about.xml file.
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //This corresponds to the onCreate that we have in our
